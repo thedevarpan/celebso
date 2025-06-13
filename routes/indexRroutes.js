3d0135d8d28fdeae662293730e94acb18cdcf746
Arpan <me.arpan122@gmail.com>
2025-06-13 11:12:20 +0530

    about linked

--- routes/indexRroutes.js
+++ routes/indexRroutes.js
@@ -6,7 +6,7 @@ router.get("/", (req, res) => {
   res.render("index", {
     metaTitle: "Home - celebso | Best Social Media For Influiencers",
     metaDescription: "Celebso is the ultimate platform for Entrepreneur, Startups and influencers to connect, collaborate, and grow together.",
-    metaKeywords: "Startups, Platform for Entrepreneur, Influencers social media, Social Media For Influiencers ",
+    metaKeywords: "Startups, Platform for Entrepreneur, Influencers social media, Social Media For Influiencers",
     ogImage: "/assets/images/banner.jpg",
     pageUrl: req.protocol + "://" + req.get("host") + req.originalUrl
   });
