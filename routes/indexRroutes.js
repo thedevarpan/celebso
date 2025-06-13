7dd4e8034ecdba9efb97d0a482a7e136fb6aaedc
Arpan <me.arpan122@gmail.com>
2025-06-13 10:49:54 +0530

    seo keywords update

--- routes/indexRroutes.js
+++ routes/indexRroutes.js
@@ -5,9 +5,9 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.render("index", {
     metaTitle: "Home - celebso | Best Social Media For Influiencers",
-    metaDescription: "We build high-performance websites with Node.js, EJS, MongoDB, and Tailwind CSS. Contact us for professional web solutions.",
-    metaKeywords: "web development, node.js, ejs, mongodb, flowmazon, tailwind, seo",
-    ogImage: "https://yourdomain.com/assets/images/banner.jpg",
+    metaDescription: "Celebso is the ultimate platform for Entrepreneur, Startups and influencers to connect, collaborate, and grow together.",
+    metaKeywords: "Startups, Platform for Entrepreneur, Influencers social media, Social Media For Influiencers ",
+    ogImage: "/assets/images/banner.jpg",
     pageUrl: req.protocol + "://" + req.get("host") + req.originalUrl
   });
 });
